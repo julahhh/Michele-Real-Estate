5d7ed8c5fcbf807dace818791dddc0f523b63467
Julia M. Chickonski <julia.chickonski@gmail.com>
2026-01-26 19:10:05 -0500

    Fixed background color on the contact page in the style sheet. also added headers and footers to About, buy, sell, and blog.

--- site/blog.php
+++ site/blog.php
@@ -1,6 +1,6 @@
 <?php
-$pageTitle = "Sell";
-$activePage = "sell";
+$pageTitle = "Blog Home";
+$activePage = "blog";
 
 require __DIR__ . "/partials/head.php";
 require __DIR__ . "/partials/header.php";
--- site/buy.php
+++ site/buy.php
@@ -1,15 +1,10 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>PHP Plain Text Example</title>
-</head>
-<body>
-    <h1>My Website Content</h1>
-    <?php
-        // This is a PHP code block
-        echo "<p>This is plain text output using PHP's echo statement.</p>";
-        echo "<p>Newlines in the source code are ignored by the browser, 
-              but you can use HTML tags like <br> to force a break.</p>";
-    ?>
-</body>
-</html>
+<?php
+$pageTitle = "Buy";
+$activePage = "buy";
+
+require __DIR__ . "/partials/head.php";
+require __DIR__ . "/partials/header.php";
+
+?>
+
+<?php require __DIR__ . "/partials/footer.php"; ?>
